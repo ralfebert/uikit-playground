ca5b14b22eeebefc9e76520c284ce9a8da3b3ec7
Ralf Ebert <info@ralfebert.de>
2015-10-10 21:07:28 +0200

    colored uiprogressview

--- UIProgressView.playground/Contents.swift
+++ UIProgressView.playground/Contents.swift
@@ -1,5 +1,9 @@
 import UIKit
 
 let progressView = UIProgressView(progressViewStyle: UIProgressViewStyle.Default)
-progressView.progress = 0.5
+progressView.progress = 50 / Float(100)
 
+let coloredProgressView = UIProgressView(progressViewStyle: .Default)
+coloredProgressView.progressTintColor = .redColor()
+coloredProgressView.trackTintColor = .yellowColor()
+coloredProgressView.progress = 0.25
