42d8228de2d119c963638516c9a0e778ceecba9f
Ralf Ebert <ralf.ebert@gmail.com>
2017-11-08 16:40:09 +0100

    cleanup

--- UIKit.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-import XCPlayground
-
-let webView = UIWebView(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
-webView.loadHTMLString("<i>Hello world</i>", baseURL: nil)
-
-// Show in playground timeline
-import XCPlayground
-XCPShowView("WebView", view: webView)
